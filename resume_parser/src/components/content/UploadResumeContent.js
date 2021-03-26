5c77aa36e43e3ec8e6715e84c77ccbee5d7752dc
Vijay <may31.py@gmail.com>
2021-03-26 10:48:34 +0530

    Added Drag/Drop feature

--- resume_parser/src/components/content/UploadResumeContent.js
+++ resume_parser/src/components/content/UploadResumeContent.js
@@ -1,11 +1,60 @@
+import { Button, Card, CardContent, Grid } from '@material-ui/core';
+import { Form, Formik } from 'formik';  
+
 import React, { PureComponent } from 'react'
+import { array, object, string } from 'yup';
+import { MultipleFileUploadField } from '../../upload/MultipleFileUploadField';
+
+
+
+
 
 export class UploadResumeContent extends PureComponent {
     render() {
         return (
             <div>
 
-                <h1>upload your resume content here!</h1>
+<Card>
+      <CardContent>
+        <Formik
+          initialValues={{ files: [] }}
+          validationSchema={object({
+            files: array(
+              object({
+                url: string().required(),
+              })
+            ),
+          })}
+          onSubmit={(values) => {
+            console.log('values', values);
+            return new Promise((res) => setTimeout(res, 2000));
+          }}
+        >
+          {({ values, errors, isValid, isSubmitting }) => (
+            <Form>
+              <Grid container spacing={3} direction="column" justify = "center" >
+                <MultipleFileUploadField name="files" />
+
+                <Grid item >
+                  <Button
+                  
+                  
+                    variant="contained"
+                    color="primary"
+                    disabled={!isValid || isSubmitting}
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </CardContent>
+    </Card>
                 
             </div>
         )
